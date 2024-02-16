31c9f12b2b9370d096a90ebc8e2dc84aaf7a83a1
Korbinian Eckstein <korbinian90@gmail.com>
2024-02-16 19:51:41 +0100

    copy logfile and remove tmp dir

--- Part1/InstallProgramPaths.sh
+++ Part1/InstallProgramPaths.sh
@@ -1,18 +1,5 @@
+#!/bin/bash
 ##### In this file aliases and paths can be stored so that the program knows all commands.
-
-##### The following programs are necessary:
-##### -- OS: Ubuntu 12.04			(12.04.3 LTS, current used Kernel: GNU/Linux 3.2.0-48-generic x86_64)
-##### -- Minc 						(program: 2.0.18, libminc: 2.0.18, netcdf: 3.6.3, HDF5: 1.6.6,)
-##### -- MATLAB 					(matlab78R2009a)
-##### -- BET 						(of FSL package 4.1, 2008)
-##### -- tar 						(Any version should work)
-##### -- gzip						(Any version should work)
-##### -- gunzip						(Any version should work)
-##### -- LCModel 					(Version 6.3.1)
-
-
-
-# aliases
 
 # MATLAB Runtime (runs compiled MATLAB code without required license)
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/MATLAB_Runtime_R2021b/v911/runtime/glnxa64:/opt/MATLAB_Runtime_R2021b/v911/bin/glnxa64:/opt/MATLAB_Runtime_R2021b/v911/sys/os/glnxa64:/opt/MATLAB_Runtime_R2021b/v911/sys/opengl/lib/glnxa64
@@ -26,7 +13,16 @@ export betp=bet
 # LCModel Path
 export LCM_Path=lcmodel
 
-export RunLCModelOn=$(hostname)		# Run LCModel on different computer, connecting via ssh. You need a key so that you can automatically connect to this
-									# computer, without needing to type in the password!
-									# BE AWARE THAT THIS COMPUTER HAS TO BE ABLE TO ACCESS THE "LCM_Path", THE BASIS-FILE AND THE "out_path"!
-#export RunLCModelAs=""				# If you need to be a specific user on the LCModel computer. Leave empty (or dont declare it at all) if not necessary.
+# Run LCModel on different computer, connecting via ssh. You need a key so that you can automatically connect to this
+# computer, without needing to type in the password!
+# BE AWARE THAT THIS COMPUTER HAS TO BE ABLE TO ACCESS THE "LCM_Path", THE BASIS-FILE AND THE "out_path"!
+RunLCModelOn=$(hostname)
+export RunLCModelOn
+# If you need to be a specific user on the LCModel computer. Leave empty (or dont declare it at all) if not necessary.
+# export RunLCModelAs=""
+
+# Path of local matlab for running the not_compiled version
+export matlabp=matlab
+
+# MATLAB Functions Folder for running the not_compiled version
+export MatlabFunctionsFolder="/neurodesktop-storage/mrsi_processing/Part1_Reco_LCModel_MUSICAL_newREAD_vb17_7T_CSI/Matlab_Functions"
--- Part1/Part1_ProcessMRSI.sh
+++ Part1/Part1_ProcessMRSI.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 ###################################################################################################
 ###	Process 2D/3D/Multislice CSI data, prepare them for LCModel Processing and start LCModel	###
-###	   Data can be miltichannel data in which case GRE/reference imaging data is used to	###
-###	                             combine the individual channels.	      		  	###
+###	   Data can be miltichannel data in which case GRE/reference imaging data is used to		###
+###	                             combine the individual channels.	      		  				###
 ###	Data can also be undersampled, in which case GRAPPA-based 2D-Caipirinha and 1D-Caipirinha	###
-###						algorithms are used for Reco.						###
+###								algorithms are used for Reco.									###
 ###################################################################################################
-
 
 # -1. Preparations
 # In case you hit ctrl-c, kill all processes, also background processes. Trap all exit signals and the SIGUSR1 signal.
 trap "{ Trapped=1; echo Try to kill: PID: $$, PPID: $PPID; TerminateProgram; echo 'Kill all processes due to user request.'; sleep 3s; kill 0;}" SIGINT SIGUSR1
-TerminateProgram(){
-
+TerminateProgram() {
 
 	echo "Stop tee."
 	# close and restore backup; both stdout and stderr
@@ -25,112 +23,91 @@ TerminateProgram(){
 
 	echo -e "\n\nTerminate Program & Backup: "
 
-	cd $calldir
+	cd "$calldir" || exit
 
 	# Copy the logfile
-	echo "Copy logfile to $out_path/UsedSourcecode/logfile.log."
-	if [ -d $out_path/UsedSourcecode ]; then
-		cp $logfile $out_path/UsedSourcecode
-		cp $tmp_dir/ErrorFile.sh $out_path/UsedSourcecode
-	fi
+	echo "Copy logfile to $out_path"
+	cp "$logfile" "$out_path"
+	cp "$tmp_dir/ErrorFile.sh" "$out_path"
 
+	rm -fR "$out_path/TempServerDir"
 
-	if [ -f "$out_path/TempServerDir/KillLCMProcesses.sh" ]; then
-		if [[ $RunLCModelOn == "" ]] || [[ $CurrentComputer == $RunLCModelOn ]]; then
-			$out_path/TempServerDir/KillLCMProcesses.sh														# kill locally
-		else
-			if [[ $RunLCModelAs == "" ]]; then
-				ssh -o PasswordAuthentication=no $RunLCModelOn "$out_path/TempServerDir/KillLCMProcesses.sh"
-			else
-				ssh -o PasswordAuthentication=no -l $RunLCModelAs $RunLCModelOn "$out_path/TempServerDir/KillLCMProcesses.sh"
-			fi
-		fi
-	fi
-	rm -fR $out_path/TempServerDir
-
-	if [[ "$1" == "1" ]]; then		# DebugFlag = 1
+	if [[ "$1" == "1" ]]; then # DebugFlag = 1
 		rmtmpdir="n"
 	else
-		rmtmpdir="y"		
+		rmtmpdir="y"
 	fi
 	echo -e "\n$rmtmpdir"
 	if [[ "$rmtmpdir" == "y" ]]; then
 
 		if [ -n "${pid_list[1]}" ]; then
 			echo "Wait for all LCModel Processes to terminate gracefully."
-			sleep 40		# So that all lcmodel processes can close
+			sleep 40 # So that all lcmodel processes can close
 		fi
 
-		rm -R -f "$( dirname "${BASH_SOURCE[0]}" )/$tmp_dir"
-		if [ -d "$( dirname "${BASH_SOURCE[0]}" )/$tmp_dir" ]; then
+		rm -R -f "$tmp_dir"
+		if [ -d "$tmp_dir" ]; then
 			sleep 10
-			rm -R -f "$( dirname "${BASH_SOURCE[0]}" )/$tmp_dir"		# Try again if it didnt work
+			rm -R -f "$tmp_dir" # Try again if it didnt work
 		fi
 	fi
 	echo "Stop now."
 	echo -e "\n\n\n\t\tE N D\n\n\n"
 
 	if [[ "$Trapped" == "0" ]]; then
-		exit 0;
+		exit 0
 	fi
 }
 
 # -1.1 Debug flag
-DebugFlag=0
+DebugFlag=1
 Trapped=0
-
 
 # -1.2 Change to directory of script
 calldir=$(pwd)
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 # -1.3 Create directories
 tmp_trunk="${calldir}/tmp"
 tmp_num=1
 tmp_dir="${tmp_trunk}${tmp_num}"
 while [ -d "$tmp_dir" ]; do
-	let tmp_num=tmp_num+1
-	tmp_dir="${tmp_trunk}${tmp_num}"	
+	((tmp_num = tmp_num + 1))
+	tmp_dir="${tmp_trunk}${tmp_num}"
 done
 export tmp_dir
-mkdir $tmp_dir
-chmod 775 $tmp_dir
-abs_tmp_dir=$(readlink -f $tmp_dir)
-
+mkdir "$tmp_dir"
+chmod 775 "$tmp_dir"
+abs_tmp_dir=$(readlink -f "$tmp_dir")
 
 # -1.4 Write the script output to a logfile
 logfile=${tmp_dir}/logfile.log
 hostyy=$(hostname)
 echo -e "Run Script on $hostyy with parameters:\n$0 $*\n\n"
-echo -e "Run Script on $hostyy with parameters:\n$0 $*" > $logfile
-
+echo -e "Run Script on $hostyy with parameters:\n$0 $*" >"$logfile"
 
 # backup the original filedescriptors, first
 # stdout (1) into fd6; stderr (2) into fd7
-exec 6<&1							# Copy 1 to 6
-exec 7<&2							# Copy 2 to 7
-exec > >(tee -a $logfile)			# Copy 1 to tee which writes to logfile
-exec 2> >(tee -a $logfile >&2)		# Copy 2 to tee which writes to logfile and redirect to 1 and 2 [???]
-
-
-
-
+exec 6<&1                        # Copy 1 to 6
+exec 7<&2                        # Copy 2 to 7
+exec > >(tee -a "$logfile")      # Copy 1 to tee which writes to logfile
+exec 2> >(tee -a "$logfile" >&2) # Copy 2 to tee which writes to logfile and redirect to 1 and 2 [???]
 
 echo -e "\n\n0.\t\tS T A R T (PID $$, PPID $PPID)"
-sleep 3s;
+sleep 3s
+
 # 0.
 ############# DEFINE ARGUMENTS/PARAMETER OPTIONS ####################
-
+# export so that every child process (like readcsi.sh), grandchild-process etc. can use these variables
 
 # FLAGS
 # mandatory
-export csi_flag=0 			   #export so that every child process (like readcsi.sh), grandchild-process etc. can use this variable
+export csi_flag=0
 export basis_flag=0
 export out_flag=0
 
 # optional
-export image_normal_flag=0  			 
+export image_normal_flag=0
 export image_flip_flag=0
 export image_VC_flag=0
 export T1w_flag=0
@@ -157,7 +134,6 @@ export XPACE_motion_correction_flag=0
 export julia_reconstruction=0
 export old_dat_file_flag=0
 
-
 # INITIALIZING
 export phase_encoding_direction_is_RL_flag=0
 export phase_encoding_direction_is="AP"
@@ -165,103 +141,130 @@ LipidDecon_MethodAndNoOfLoops="L1,10"
 export julia_n_threads="auto"
 export julia_mmap="false"
 
-
-while getopts 'c:b:o:i:f:v:t:a:w:W:m:h:L:r:R:g:Fuz:I:A:lj:e:B:J:X:s:d?' OPTION
-do
+while getopts 'c:b:o:i:f:v:t:a:w:W:m:h:L:r:R:g:Fuz:I:A:lj:e:B:J:X:s:d?' OPTION; do
 	case $OPTION in
 
-#mandatory
-	  c)	export csi_flag=1
-			export csi_path="$OPTARG"
-			;;
-	  b)	export basis_flag=1
-			export basis_path="$OPTARG"
-			;;
-	  o)	export out_flag=1
-			export out_path="$OPTARG"
-			;;
+	#mandatory
+	c)
+		export csi_flag=1
+		export csi_path="$OPTARG"
+		;;
+	b)
+		export basis_flag=1
+		export basis_path="$OPTARG"
+		;;
+	o)
+		export out_flag=1
+		export out_path="$OPTARG"
+		;;
 
-
-#optional
-	  i)	export image_normal_flag=1				
-			export image_normal_path="$OPTARG"
-			;;
-	  f)	export image_flip_flag=1				
-			export image_flip_path="$OPTARG"
-			;;
-	  v)	export image_VC_flag=1
-			export image_VC_path="$OPTARG"
-			;;
-	  t)	export T1w_flag=1
-			export T1w_path="$OPTARG"
-			;;
-	  a)	export T1w_AntiNoise_flag=1
-	  		export T1w_AntiNoise_path="$OPTARG"
-	  		;;
-	  w)	export WaterReference_flag=1
-	  		export WaterReference_MethodAndFile="$OPTARG"
-	  		;;
-	  W)	export LCM_ControlPath_Water_flag=1
-	  		export LCM_Control_Water_path="$OPTARG"
-	  		;;
-	  m)	export mask_flag=1
-			export mask_method="$OPTARG"
-			;;
-	  h)	export hamming_flag=1
-			export hamming_factor="$OPTARG"
-			;;
-	  L)	export LipidDecon_flag=1
-			export LipidDecon_MethodAndNoOfLoops="$OPTARG"
-			;;
-	  r)	export TwoDCaipParallelImaging_flag=1
-			export InPlaneCaipPattern_And_VD_Radius="$OPTARG"
-			;;
-	  R)	export SliceParallelImaging_flag=1
-			export SliceAliasingPattern="$OPTARG"
-			;;
-	  g)	export noisedecorrelation_flag=1
-			export noisedecorrelation_path="$OPTARG"
-			;;
-	  F)	export FirstOrderPhaseCorr_flag=1
-	  		;;
-	  u)	export use_phantom_flag=1
-			;;
-	  z)	export ZeroFillMetMaps_flag=1	
-			export ZeroFillMetMaps="$OPTARG"
-			;;
-	  I)	export InterpolateCSIResolution_flag=1
-			export InterpolateCSIResolution="$OPTARG"
-			;;
-	  A)	export AlignFrequency_flag=1
-			export AlignFrequency_path="$OPTARG"
-			;;
-	  l)	export dont_compute_LCM_flag=1
-			;;
-	  j)	export LCM_ControlPath_flag=1
-			export LCM_ControlPath="$OPTARG"
-			;;
-	  e)	export exponential_filter_Hz_flag=1
-			export exponential_filter_Hz="$OPTARG"
-			;;
-	  B)	export basis_echo_flag=1
-			export basis_echo_path="$OPTARG"
-			;;
-	  J)	export control_echo_flag=1
-			export control_echo_path="$OPTARG"
-			;;
-	  X)	export XPACE_motion_correction_flag=1
-			export XPACE_motion_correction_path="$OPTARG"
-			;;
-	  s)	export julia_reconstruction=1
-	  		export julia_n_threads="$OPTARG"
-			julia_mmap=${!OPTIND}
-			if [[ -z $julia_mmap ]]; then
-				julia_mmap="false"
-			fi
-	  		;;
-	  d)	export old_dat_file_flag=1
-	  		;;
-	  ?)	printf "
+	#optional
+	i)
+		export image_normal_flag=1
+		export image_normal_path="$OPTARG"
+		;;
+	f)
+		export image_flip_flag=1
+		export image_flip_path="$OPTARG"
+		;;
+	v)
+		export image_VC_flag=1
+		export image_VC_path="$OPTARG"
+		;;
+	t)
+		export T1w_flag=1
+		export T1w_path="$OPTARG"
+		;;
+	a)
+		export T1w_AntiNoise_flag=1
+		export T1w_AntiNoise_path="$OPTARG"
+		;;
+	w)
+		export WaterReference_flag=1
+		export WaterReference_MethodAndFile="$OPTARG"
+		;;
+	W)
+		export LCM_ControlPath_Water_flag=1
+		export LCM_Control_Water_path="$OPTARG"
+		;;
+	m)
+		export mask_flag=1
+		export mask_method="$OPTARG"
+		;;
+	h)
+		export hamming_flag=1
+		export hamming_factor="$OPTARG"
+		;;
+	L)
+		export LipidDecon_flag=1
+		export LipidDecon_MethodAndNoOfLoops="$OPTARG"
+		;;
+	r)
+		export TwoDCaipParallelImaging_flag=1
+		export InPlaneCaipPattern_And_VD_Radius="$OPTARG"
+		;;
+	R)
+		export SliceParallelImaging_flag=1
+		export SliceAliasingPattern="$OPTARG"
+		;;
+	g)
+		export noisedecorrelation_flag=1
+		export noisedecorrelation_path="$OPTARG"
+		;;
+	F)
+		export FirstOrderPhaseCorr_flag=1
+		;;
+	u)
+		export use_phantom_flag=1
+		;;
+	z)
+		export ZeroFillMetMaps_flag=1
+		export ZeroFillMetMaps="$OPTARG"
+		;;
+	I)
+		export InterpolateCSIResolution_flag=1
+		export InterpolateCSIResolution="$OPTARG"
+		;;
+	A)
+		export AlignFrequency_flag=1
+		export AlignFrequency_path="$OPTARG"
+		;;
+	l)
+		export dont_compute_LCM_flag=1
+		;;
+	j)
+		export LCM_ControlPath_flag=1
+		export LCM_ControlPath="$OPTARG"
+		;;
+	e)
+		export exponential_filter_Hz_flag=1
+		export exponential_filter_Hz="$OPTARG"
+		;;
+	B)
+		export basis_echo_flag=1
+		export basis_echo_path="$OPTARG"
+		;;
+	J)
+		export control_echo_flag=1
+		export control_echo_path="$OPTARG"
+		;;
+	X)
+		export XPACE_motion_correction_flag=1
+		export XPACE_motion_correction_path="$OPTARG"
+		;;
+	s)
+		export julia_reconstruction=1
+		export julia_n_threads="$OPTARG"
+		julia_mmap=${!OPTIND}
+		if [[ -z $julia_mmap ]]; then
+			julia_mmap="false"
+		fi
+		;;
+	d)
+		export old_dat_file_flag=1
+		;;
+	?)
+		printf "
 
 Usage: %s
 
@@ -300,87 +303,58 @@ optional:
 
 " $(basename $0) >&2
 
-			exit 2
-			;;
+		exit 2
+		;;
 	esac
 done
 
-
-shift $(($OPTIND - 1))
-
+shift $((OPTIND - 1))
 
 ###0. GH: measure time elapsed:
-
 START=$(date +%s.%N)
 
-
-
 # 1. Install Paths etc.
-echo -e "\n\n1. Install Program\n\n" 
- . ./InstallProgramPaths.sh
-
-
+echo -e "\n\n1. Install Program\n\n"
+. ./InstallProgramPaths.sh
 
 # 2. Create Out Directories
-rm -R ${out_path}/*.mat; rm -R ${out_path}/scalings/*.mat
-mkdir -p ${out_path}/maps
-mkdir ${out_path}/phamaps
-mkdir ${out_path}/spectra
-mkdir ${out_path}/scalings
-
-
-
-#if [ -d "$out_path" ]; then
-#	echo -e "\n\nWARNING: The directory\n$out_path\ndoes exist.\nd\t...\tdelete directory,\nc\t...\tcontinue with process without deleting (overwrite files),\ne\t...\tstop program\n\n"
-#	read Proceed
-#	if [[ "$Proceed" == "d" ]]; then
-#		rm -r $out_path
-#	elif [[ "$Proceed" == "e" ]]; then
-#		exit
-#	fi
-#fi
-
-
-
+rm -R "$out_path/"*.mat
+rm -R "$out_path/scalings/"*.mat
+mkdir -p "$out_path/maps"
+mkdir "$out_path/phamaps"
+mkdir "$out_path/spectra"
+mkdir "$out_path/scalings"
 
 # 3. Write Initial Parameters
+echo -e "\n\n3. Write Initial Parameters\n\n"
+. ./write_InitialParameters.sh
 
-echo -e "\n\n3. Write Initial Parameters\n\n" 
- . ./write_InitialParameters.sh
-
-
-
-#read -p "Stop before Gathering info."
+# read -rp "Stop before Gathering info."
 # 4.
 ############ CREATE TEMPLATES FOR CREATING METABOLIC MAPS LATER ############
-
-echo -e "\n\n4. Gather Information, Create Minc Templates, Prepare Mask Creation.\n\n" 
-if [[ $LCM_ControlPath_Water_flag -eq 1 ]]; then		# Run it twice for water referencing (it will automatically process the water ref file the first time, and the other the second time)
-								# The condition is fulfilled if Control file for water is set - W2 water reference processing
-	$Matlab_Compiled/GetPar_CreateTempl_MaskPart1 $abs_tmp_dir
+echo -e "\n\n4. Gather Information, Create Minc Templates, Prepare Mask Creation.\n\n"
+# Run it twice for water referencing (it will automatically process the water ref file the first time, and the other the second time)
+if [[ $LCM_ControlPath_Water_flag -eq 1 ]]; then
+	"$Matlab_Compiled/GetPar_CreateTempl_MaskPart1" "$abs_tmp_dir"
 fi
+"$Matlab_Compiled/GetPar_CreateTempl_MaskPart1" "$abs_tmp_dir"
 
-$Matlab_Compiled/GetPar_CreateTempl_MaskPart1 $abs_tmp_dir
- . ${tmp_dir}/ErrorFile.sh
+# Terminate if there was an error
+. "$tmp_dir/ErrorFile.sh"
 if [[ $ErrorInGetPar_CreateTempl -eq 1 ]]; then
 	TerminateProgram $DebugFlag
 fi
 
-#read -p "stop before create minc template"
-bash ${tmp_dir}/CreateMincTemplates.sh
+# read -rp "stop before create minc template"
+bash "$tmp_dir/CreateMincTemplates.sh"
 
-
-
-
-#read -p "Stop before creating mask."
+# read -rp "Stop before creating mask."
 ## 5.
 ############# USE MASK OR CREATE MASK OUT OF (IN PRIORITY ORDER): MASK, T1_MAP, IMAGING AC, IMAGING VC, CSI ############
-
-echo -e "\n\n5. CREATE MASK\n\n" 
+echo -e "\n\n5. CREATE MASK\n\n"
 ./create_mask.sh
 
-
-#read -p "Stop before MRSI_Reconstruction.m"
+# read -rp "Stop before MRSI_Reconstruction.m"
 # 6.
 ###########   Process Data, Prepare LCModel Fitting   ############
 echo -e "\n\n\n6. Process Data and Prepare LCModel Processing\n\n"
@@ -391,107 +365,63 @@ if [[ $julia_reconstruction -eq 1 ]]; then
 		julia_decon="false"
 	fi
 	echo -e "\nMRSI.jl reconstruction"
-	julia --threads=$julia_n_threads julia_reco.jl $old_dat_file_flag $csi_path $tmp_dir $out_path $julia_decon $julia_mmap
-	echo -e "\nWriting LCM files"
-	$Matlab_Compiled/julia_write_lcm_files $abs_tmp_dir
+	(
+		set -x # Print the command that is being executed
+		julia "--threads=$julia_n_threads" julia_reco.jl $old_dat_file_flag "$csi_path" "$tmp_dir" "$out_path" "$julia_decon" "$julia_mmap"
+		"$Matlab_Compiled/julia_write_lcm_files" "$abs_tmp_dir"
+	)
 else
-	if [[ $WaterReference_flag -eq 1 ]]; then		# Run it twice for water referencing (it will automatically process the water ref file the first time, and the other the second time)
+	# Run it twice for water referencing (it will automatically process the water ref file the first time, and the other the second time)
+	if [[ $WaterReference_flag -eq 1 ]]; then
 		echo -e "\nProcess water reference data for water scaling."
-		echo -e "\n\nRunning:\n"; echo "\"$Matlab_Compiled/MRSI_Reconstruction $abs_tmp_dir 1\""
-		$Matlab_Compiled/MRSI_Reconstruction $abs_tmp_dir 1
+		echo -e "\n\nRunning:\n"
+		(
+			set -x # Print the command that is being executed
+			"$Matlab_Compiled/MRSI_Reconstruction" "$abs_tmp_dir" 1
+		)
 	fi
-	echo -e "\n\nRunning:\n"; echo "\"$Matlab_Compiled/MRSI_Reconstruction $abs_tmp_dir 1\""
-	$Matlab_Compiled/MRSI_Reconstruction $abs_tmp_dir 1 # If we pass over several IMA or dat files, average them
+	echo -e "\n\nRunning:\n"
+	(
+		set -x # Print the command that is being executed
+		"$Matlab_Compiled/MRSI_Reconstruction" "$abs_tmp_dir" 1 # If we pass over several IMA or dat files, average them
+	)
 fi
 
-
-
-#read -p "Stop before LCModel fitting"
+# read -rp "Stop before LCModel fitting"
 #7.
 ########### START LCMODEL PROCESSING OF SINGLE VOXEL DATA ON CPU CORES ############
 if [[ $dont_compute_LCM_flag -eq 0 ]]; then
 	curdir=$(pwd)
 	CurrentComputer=$(hostname)
-	if [[ $RunLCModelOn == "" ]] || [[ $CurrentComputer == $RunLCModelOn ]]; then
-		./RunLCModel.sh $RunLCModelOn $tmp_dir														# Run LCModel locally
+	if [[ "$RunLCModelOn" == "" ]] || [[ $CurrentComputer == "$RunLCModelOn" ]]; then
+		./RunLCModel.sh "$RunLCModelOn" "$tmp_dir" # Run LCModel locally
 	else
-		rm -fR $out_path/TempServerDir
-		cp -R $tmp_dir/ $out_path/TempServerDir; RunFileOnServer=$out_path/TempServerDir/RunLCModel.sh; cp $curdir/RunLCModel.sh $RunFileOnServer
-		if [[ $RunLCModelAs == "" ]]; then																									 # Run LCModel on different computer,
-			ssh -o PasswordAuthentication=no $RunLCModelOn "$RunFileOnServer $RunLCModelOn $out_path/TempServerDir"					 # connecting via ssh. You need 
-		else																																 # a key so that you can
-			ssh -o PasswordAuthentication=no -l $RunLCModelAs $RunLCModelOn "$RunFileOnServer $RunLCModelOn $out_path/TempServerDir" # automatically connect to this computer,
-		fi																																	 # without needing to type in the password!
+		rm -fR "$out_path/TempServerDir"
+		cp -R "$tmp_dir/" "$out_path/TempServerDir"
+		RunFileOnServer=$out_path/TempServerDir/RunLCModel.sh
+		cp "$curdir/RunLCModel.sh" "$RunFileOnServer"
+		# Run LCModel on different computer,connecting via ssh. You need a key so that you can automatically connect to this computer, without needing to type in the password!
+		if [[ "$RunLCModelAs" == "" ]]; then
+			ssh -o PasswordAuthentication=no "$RunLCModelOn" "$RunFileOnServer $RunLCModelOn $out_path/TempServerDir"
+		else
+			ssh -o PasswordAuthentication=no -l "$RunLCModelAs" "$RunLCModelOn" "$RunFileOnServer $RunLCModelOn $out_path/TempServerDir"
+		fi
 		sleep 40
-		rm -fR $out_path/TempServerDir
+		rm -fR "$out_path/TempServerDir"
 	fi
 	sleep 10
 fi
 
-
-
 #8a: GH: finish time measurement
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
-echo -e "\n\n8. Total Processing time of Part 1: " $DIFF "\n\n"
-
-
-
-# # 8.
-# ############ WRITE THE SOURCECODE THAT WAS USED TO OUT-DIR ############
-# echo -e "\n\n8. Write the used sourcecode to out-dir.\n\n"
-
-# # Copy Program where this program lies in
-# curdir=$(pwd)
-# curdir_folder=${curdir##*/}
-# mkdir -p "$out_path/UsedSourcecode/$curdir_folder"
-# cp -R $curdir/* $out_path/UsedSourcecode/$curdir_folder
-# rm -R $out_path/UsedSourcecode/$curdir_folder/${tmp_dir}
-
-# ## Copy the Run-files one level above the program
-# #abovecurdir=${curdir%/*}	# Delete the thing that follows the last /
-# #cp $abovecurdir/*.sh $out_path/UsedSourcecode
-
-# # Copy the logfile
-# cp $logfile $out_path/UsedSourcecode
-
-# # Copy Matlab Functions
-# MatlabFolderFound=$(find $out_path/UsedSourcecode -maxdepth 2 -type d -name 'Matlab_Functions' | grep -c Matlab_Functions)
-# if [[ $MatlabFolderFound -eq 0 ]]; then
-# 	cp -R $MatlabFunctionsFolder $out_path/UsedSourcecode
-# fi
-
-# ## Remove all backup-files and git-folders
-# #find $out_path/UsedSourcecode -name \*.*~ -type f -delete 2> /dev/null
-# #find $out_path/UsedSourcecode -name \*.git -type d -exec rm -r {} \; 2> /dev/null
-
-# # Compress the folder
-# if [ -e $out_path/UsedSourcecode_Part1.tar.gz ]; then
-# 	rm $out_path/UsedSourcecode_Part1.tar.gz
-# fi
-# cd $out_path
-# tar cfz $out_path/UsedSourcecode_Part1.tar.gz UsedSourcecode
-
-# # Go back to the original folder
-# cd $curdir
-
-# # Delete the unzipped stuff
-# rm -R -f $out_path/UsedSourcecode
-
-
-
+echo -e "\n\n8. Total Processing time of Part 1: " "$DIFF" "\n\n"
 
 #9.
 ############ REMOVE UNECESSARY DATA ############
 if [[ $DebugFlag -eq 0 ]]; then
 	echo -e "\n\n9. Remove unnecessary data!\n\n"
-	rm $out_path/spectra*/*.RAW 
-	rm $out_path/spectra*/*.control
-	rm $out_path/water_spectra/*.RAW 
-	rm $out_path/water_spectra/*.control 
-	#rm -r $out_path/spectra/CoordFiles     #commented, coordfiles now used in wolfgang addition
-	#rm $out_path/spectra/*.CSV
+	find "$out_path" -name '*.RAW' -delete
+	find "$out_path" -name '*.control' -delete
 fi
 TerminateProgram $DebugFlag
-
-
